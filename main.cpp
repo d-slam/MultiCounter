2d3b887e069a38fd41408de49a31d47437a66bea
Dami <71833645+d-slam@users.noreply.github.com>
2023-04-04 21:37:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,120 @@
 #include <Arduino.h>
-#include <Servo.h>
 
-class ServoHold
+typedef enum
+{
+	up,
+	nix,
+	down
+} richtung;
+
+class Counter
 {
 public:
-	ServoHold(int p) : pin(p) {}
-	void setup() { mServo.attach(pin); }
-	void doWrite() { mServo.write(winkel); }
-	void setWinkel(int w) { winkel = w; }
+	void setMax(int m) { max = m; }
+	void setMin(int m) { min = m; }
+	void setStatus(richtung r) { mRichtung = r; }
+	richtung getStatus() { return mRichtung; }
+	int getCurrent() { return current; }
 
-	bool fkt(int goTill)
+
+	richtung countTo(richtung r)
 	{
-		static int count = goTill;
-
-		count--;
-		if (count <= 0)
+		if (r == nix)
 		{
-			count = 0;
-			return false;
+			return nix;
 		}
-		else
+		if (r == up)
 		{
-			return fkt(count);
+			if (current >= max)
+			{
+				return nix;
+			}
+			else
+			{
+				current++;
+				return up;
+			}
+		}
+		if (r == down)
+		{
+			if (current <= min) // ende
+			{
+				return nix;
+			}
+			else
+			{
+				current--;
+				return down; // in progress!!
+			}
 		}
 	}
 
+
 private:
-	int pin;
-	int winkel;
-	Servo mServo;
+	richtung mRichtung = nix;
+	int max = 0;
+	int min = 0;
+
+	int neu = 90;
+	int current = neu;
 };
 
-ServoHold GierPlaneServo(6);
+Counter countAlf;
+Counter countBob;
 
 void setup()
 {
 	Serial.begin(9600);
-	Serial.println("serial da!");
-	GierPlaneServo.setup();
+	Serial.println("bello bellt!");
+
+	countAlf.setMax(166);
+	countAlf.setMin(10);
+
+	countBob.setMax(178);
+	countBob.setMin(15);
+}
+
+richtung rFlagAlf = down;
+richtung rFlagBob = up;
+
+void serialHelper(Counter &m, String who)
+{
+	Serial.print(who);
+	Serial.print("...");
+	Serial.print(m.getCurrent());
+	Serial.println();
 }
 
 void loop()
 {
-	if (GierPlaneServo.fkt(50) == false)
+
+	if (rFlagAlf != nix)
 	{
-		GierPlaneServo.fkt(50);
+		if (rFlagAlf == up)
+		{
+			serialHelper(countAlf, "Alf up");
+			rFlagAlf = countAlf.countTo(up);
+		}
+		else if (rFlagAlf == down)
+		{
+			serialHelper(countAlf, "Alf down");
+			rFlagAlf = countAlf.countTo(down);
+		}
 	}
-	delay(50);
+
+	if (rFlagBob != nix)
+	{
+		if (rFlagBob == up)
+		{
+			serialHelper(countBob, "Bob up");
+			rFlagBob = countBob.countTo(up);
+		}
+		else if (rFlagBob == down)
+		{
+			serialHelper(countBob, "Bob down");
+			rFlagBob = countBob.countTo(down);
+		}
+	}
+
+	delay(100);
 }
